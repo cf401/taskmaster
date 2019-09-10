04ef4f6c9c179742362beeebff16b58c9834705e
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-10 13:37:20 -0700

    hotfix for app file

---
+++ src/main/java/com/codefellows/People/Controllers/TaskController.java
@@ -0,0 +1,32 @@
+package com.codefellows.People.Controllers;
+
+import com.codefellows.People.Models.Task;
+
+import com.codefellows.People.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/taskmaster")
+public class TaskController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/customers")
+    public List<Task> getCustomers() {
+        return (List) taskRepository.findAll();
+    }
+
+    @PostMapping("/customers")
+    public Task addNewUser (@RequestBody Task task) {
+        Task c = new Task();
+        c.setName( task.getName() );
+        c.setOld( task.getOld() );
+        taskRepository.save(c);
+        return c;
+    }
+}
